b151b56f07b3d7e0c70f5ab86d0a5dec90dc0f50
Sebastian Heath <sebastian.heath@gmail.com>
2020-07-17 15:36:25 -0400

    ci

--- main.py
+++ main.py
@@ -383,6 +383,8 @@ def fulltextsearch():
         qexists = False
 
     if qexists == True:
+
+        q = q.lower()
         ftquery = """PREFIX kaaont: <http://kenchreai.org/kaa/ontology/>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
